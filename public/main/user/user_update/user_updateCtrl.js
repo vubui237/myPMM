4e509f6df2e19092b932a02e2fa9c1a0aa153c86
vubui237 <vtb0977@gmail.com>
2017-08-29 23:27:18 -0500

    Added selected view for user update

--- public/main/user/user_update/user_updateCtrl.js
+++ public/main/user/user_update/user_updateCtrl.js
@@ -1,10 +1,18 @@
 angular.module('myPMM').controller('userUpdateCtrl', function($scope, userUpdateSrvc) {
     //$scope.userInfo = $scope.test2[0], After pulling the data, put it in a list to be selectable.
+    $scope.selectedUser = null;
     $scope.getDatabaseAccounts = userUpdateSrvc.getDBAccount().then((data)=> {
         $scope.userInfos = data;
         console.log($scope.userInfos);
     })
+    $scope.submitUpdate = () => {
+        $scope.selectedUsers = $scope.selectedUser.split("+")
+        console.log($scope.selectedUsers);
+    }
     $scope.updateDetails = (authid, avatar, level, kpi) => {
+        if(level > 4 || level < 0 ) {
+            alert('Invalid Entry');
+        }
         userUpdateSrvc.pushDatabaseDetails(authid, avatar,level,kpi).then((response) => {
                     if(response) {
                         alert("User details updated successfully")
